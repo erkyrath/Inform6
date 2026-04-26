e3639f478f4a9b604bf29695fab64a7a8b4b58a4
Andrew Plotkin <zarf@ZarfLent.local>
2026-04-26 12:54:23 -0400

    Change the synthetic routine name to "ARRAY:INDEX".

--- arrays.c
+++ arrays.c
@@ -789,7 +789,7 @@ advance as part of 'Zcharacter table':", unicode);
                 {
                     char *global_name = (char *)current_array_name.data;
                     ensure_memory_list_available(&embedded_function_name, strlen(global_name)+16);
-                    sprintf(embedded_function_name.data, "%s_R%d", global_name, i);
+                    sprintf(embedded_function_name.data, "%s:%d", global_name, i);
 
                     INITAOT(&AO, CONSTANT_OT);
                     /* parse_routine() releases lexer text! */
